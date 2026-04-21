da475f1af0bcd78211516c5f5ceafede1430562f
Luis-nand0 <luisfferr21@gmail.com>
2026-04-21 18:36:55 -0300

    Estilização tela de recover

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App() {
           options={{ title: 'Cadastro' }}
         />
         <Stack.Screen
-          name="login"
+          name="Login"
           component={Login}
         />
         <Stack.Screen
